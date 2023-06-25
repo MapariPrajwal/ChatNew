9bbea2a73c5ad0300fb2e1217226ec8782c58e47
Prajwal Mapari <prajwalmapari92@gmail.com>
2023-06-25 13:43:47 +0530

    first commit

---
+++ chat_new/lib/App_Colors.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: file_names, camel_case_types
+
+import 'package:flutter/material.dart';
+class App_Colors{
+  static const Color mainAppColor=Color.fromARGB(19, 61, 95, 1);
+  static const Color firstSuggestionBoxColor=Color.fromARGB(165, 231, 244, 1);
+  static const Color secondSuggestionBoxColor=Color.fromARGB(147, 202, 135, 1);
+  static const Color thirdSuggestionBoxColor=Color.fromARGB(152, 138, 239, 1);
+  static const Color assistantbackColor=Color.fromARGB(209, 243, 249, 1);
+  static const Color borderColor=Color.fromARGB(200, 200, 200, 1);
+  static const Color blackColor=Colors.black;
+  static const Color whiteColor=Colors.white;
+}
---
+++ chat_new/lib/Feature_box.dart
@@ -0,0 +1,59 @@
+import 'package:chat_new/App_Colors.dart';
+import 'package:flutter/material.dart';
+
+class FeatureBox extends StatelessWidget {
+  final Color color;
+  final String headerText;
+  final String descriptionText;
+  const FeatureBox(
+      {super.key,
+      required this.color,
+      required this.headerText,
+      required this.descriptionText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.symmetric(
+        horizontal: 35,
+        vertical: 10,
+      ),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: const BorderRadius.all(
+          Radius.circular(15),
+        ),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20).copyWith(left: 15),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                headerText,
+                style: const TextStyle(
+                  //Add Google Font,
+                  color: App_Colors.blackColor,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(height: 3,),
+            Padding(
+              padding: const EdgeInsets.only(right: 20.0),
+              child: Text(
+                descriptionText,
+                style: const TextStyle(
+                  //Add Google Font,
+                  color: App_Colors.blackColor,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
